382138add4f526f9acb7fa0dafbb890e1834b335
Gustavo <gustavopolonio1@gmail.com>
2022-05-25 21:32:31 -0300

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+
 const server = express()
+server.use(express.json())
+server.use(cors())
+
+
 const port = process.env.PORT || 3333
 
 const clubs = require('./src/data/clubs.json')
