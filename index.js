b043f619f0841174b939c98c0f9165acc4ef2316
Gustavo <gustavopolonio1@gmail.com>
2022-05-25 21:57:27 -0300

    bla

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ import cors from 'cors'
 
 const server = express()
 
-let headers = new Headers();
-
-headers.append('Content-Type', 'application/json');
-headers.append('Accept', 'application/json');
-headers.append('Authorization', 'Basic ' + base64.encode(username + ":" +  password));
-headers.append('Origin','http://localhost:3000');
-
-server.use(cors({
-  origin: '*',
-  credentials: true,
-  optionsSuccessStatus: 200,
-  allowedHeaders: headers
-}))
 
 const port = process.env.PORT || 3000
 
