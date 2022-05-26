981675fef1a2ae3e1f4c1d6bc8899131b2f2adc0
Gustavo <gustavopolonio1@gmail.com>
2022-05-25 22:02:09 -0300

    blas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from 'express'
-import cors from 'cors'
+const express = require('express')
+const cors = require('cors')
 
 const server = express()
 
+server.use(cors())
 
 const port = process.env.PORT || 3000
 
